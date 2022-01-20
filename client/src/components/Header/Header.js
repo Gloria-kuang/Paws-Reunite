762432ed14576b296405b46bd0476a3681adb765
Gloria Kuang <gloria.kcm@gmail.com>
2022-01-19 23:44:03 -0500

    created header content

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,8 +1,27 @@
 import React from "react";
 import "./Header.scss";
+import logo from "../../assets/icons/paws-logo.jpg";
+import { AiFillHome } from "react-icons/ai";
+import { BiUserCircle } from "react-icons/bi";
 
 function Header() {
-  return <div></div>;
+  return (
+    <header className="header">
+      <img src={logo} alt="logo" className="header__logo"></img>
+      <ul className="navbar">
+        <li className="navbar__list">
+          <AiFillHome />
+          Home
+        </li>
+        <li className="navbar__list">Lost & Found</li>
+        <li className="navbar__list">Search</li>
+      </ul>
+      <div className="header__user">
+        <p className="header__user-signin">Sign In</p>
+        <BiUserCircle size={60} className="header__user-logo" />
+      </div>
+    </header>
+  );
 }
 
 export default Header;
