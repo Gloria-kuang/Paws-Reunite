a6d267ee449f2b313babeee06bdeea03f47fdeb1
Gloria Kuang <gloria.kcm@gmail.com>
2022-01-20 09:16:24 -0500

    finished header style

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -2,6 +2,8 @@ import React from "react";
 import "./Header.scss";
 import logo from "../../assets/icons/paws-logo.jpg";
 import { AiFillHome } from "react-icons/ai";
+import { GiMagnifyingGlass } from "react-icons/gi";
+import { FaMapMarkerAlt } from "react-icons/fa";
 import { BiUserCircle } from "react-icons/bi";
 
 function Header() {
@@ -10,11 +12,17 @@ function Header() {
       <img src={logo} alt="logo" className="header__logo"></img>
       <ul className="navbar">
         <li className="navbar__list">
-          <AiFillHome />
+          <AiFillHome size={28} className="header__icon" />
           Home
         </li>
-        <li className="navbar__list">Lost & Found</li>
-        <li className="navbar__list">Search</li>
+        <li className="navbar__list">
+          <GiMagnifyingGlass size={30} className="header__icon" />
+          Lost & Found
+        </li>
+        <li className="navbar__list">
+          <FaMapMarkerAlt className="header__icon" />
+          Search
+        </li>
       </ul>
       <div className="header__user">
         <p className="header__user-signin">Sign In</p>
