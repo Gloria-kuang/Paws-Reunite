a356d1be2d855d8f8794e9183787ccd584a95a46
Gloria Kuang <gloria.kcm@gmail.com>
2022-01-27 11:29:01 -0500

    finished styling of the modal

---
+++ client/src/components/AlternateButton/AlternateButton.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./AlternateButton.scss";
+
+function AlternateButton({ text }) {
+  return <button className="alternate-button">{text}</button>;
+}
+
+export default AlternateButton;
--- client/src/components/ReportCardModal/ReportCardModal.js
+++ client/src/components/ReportCardModal/ReportCardModal.js
@@ -2,6 +2,9 @@ import React from "react";
 import "./ReportCardModal.scss";
 import Modal from "react-bootstrap/Modal";
 import PrimaryButton from "../PrimaryButton/PrimaryButton";
+import Tag from "../Tag/Tag";
+import testImg from "../../assets/image/circle-pet1-min.jpg";
+import AlternateButton from "../AlternateButton/AlternateButton";
 
 function ReportCardModal(props) {
   return (
@@ -12,20 +15,43 @@ function ReportCardModal(props) {
       centered
     >
       <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-vcenter">
-          Modal heading
+        <Modal.Title
+          id="contained-modal-title-vcenter"
+          className="modal__header"
+        >
+          Lost Pet Report
         </Modal.Title>
       </Modal.Header>
-      <Modal.Body>
-        <h4>Centered Modal</h4>
-        <p>
-          Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-          dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
-          consectetur ac, vestibulum at eros.
-        </p>
+      <Modal.Body className="modal__container">
+        <div className="modal__report">
+          <div className="modal__first-line">
+            <h4 className="modal__name">
+              <strong>"Duncun" </strong> <small>Male Dog</small>
+            </h4>
+            <Tag text={"Lost"}></Tag>
+          </div>
+          <p>
+            <strong>Lost Date :</strong> 2022-01-22
+          </p>
+          <p>
+            <strong>Last Seen Address :</strong> 144 Memon Place, Markham, ON
+          </p>
+          <p>
+            <strong>Description :</strong> 5 years old male Corgi named Duncun
+            was lost. Was wearing a black collar and no chip. He's super
+            friendly to people.
+          </p>
+        </div>
+        <img src={testImg} alt="dog" className="modal__image"></img>
       </Modal.Body>
-      <Modal.Footer>
-        <PrimaryButton onClick={props.onHide}>Close</PrimaryButton>
+      <Modal.Footer className="modal__footer">
+        <div className="modal__button">
+          {/* <SecondaryButton text={"Contact"} /> */}
+          <AlternateButton text={"Contact"} />
+        </div>
+        <div className="modal__button">
+          <PrimaryButton text={"Share"} />
+        </div>
       </Modal.Footer>
     </Modal>
   );
